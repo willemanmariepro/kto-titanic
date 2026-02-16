cd114e1f9233e5bf0774e82507cfdcafc1b6645c
willemanmariepro <willeman.marie.pro@gmail.com>
2026-02-16 15:12:35 +0000

    feat: clean code mon_premier_script_avec_fonction.py

--- exercices/mon_premier_script_avec_fonction.py
+++ exercices/mon_premier_script_avec_fonction.py
@@ -1,23 +1,29 @@
 import unittest
+from typing import List
 
 """
-Count names with more than seven letters
+determine the threshold for the count
 """
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+length_threshold = 7
 
-class TestNamesMethod(unittest.TestCase):
-     def test_names(self):
-        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
+
+def count_long_names(names: List[str]) -> int:
+    """Returns the count of names longer than the defined threshold."""
+    long_names = [name for name in names if len(name) > length_threshold]
+    
+    return len(long_names)
+
+
+"""
+unit test: check the attended result --> return ok 
+"""
+class TestNameCounter(unittest.TestCase):
+    def test_count_long_names(self):
+        # sample of test
+        sample_names = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        
+        # Act & Assert
+        self.assertEqual(count_long_names(sample_names), 4)
 
 if __name__ == '__main__':
     unittest.main()
