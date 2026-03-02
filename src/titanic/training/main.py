bb61782ea67fec3f743a1bd7e94032b83f17d9da
willemanmariepro <willeman.marie.pro@gmail.com>
2026-03-02 14:50:29 +0000

    feat: script entrainement fichier python + test_local_trainig.sh

--- src/titanic/training/main.py
+++ src/titanic/training/main.py
@@ -1,11 +1,22 @@
 import logging
 
 import fire
-# TODO : Ajouter les imports manquants
+# imports fichiers python
+from titanic.training.steps.load_data import load_data
+from titanic.training.steps.validate import validate
+from titanic.training.steps.split_train_test import split_train_test
+from titanic.training.steps.train import train
+
 
 def workflow(input_data_path: str, n_estimators: int, max_depth: int, random_state: int) -> None:
     logging.warning(f"workflow input path : {input_data_path}")
-    # TODO : Implémenter le workflow
+    # workflow
+    local_path = load_data(input_data_path)
+    xtrain_path, xtest_path, ytrain_path, ytest_path = split_train_test(local_path)
+    model_path = train(xtrain_path, ytrain_path, 100, 10, 42)
+    validate(model_path, xtest_path, ytest_path)
+
+
     # TODO : Dans un second temps, démarrer le run mlflow au début de ce workflow
 
 
--- src/titanic/training/steps/train.py
+++ src/titanic/training/steps/train.py
@@ -1,5 +1,10 @@
 import logging
-# TODO : Intégrer les imports manquants
+from pathlib import Path
+import joblib
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+
 
 # TODO : Dans une second temps, récupérer le client mlflow nous permettant de télécharger les artifacts enregistrés à l'étape précédente
 
@@ -9,7 +14,25 @@ ARTIFACT_PATH = "model_trained"
 def train(x_train_path: str, y_train_path: str, n_estimators: int, max_depth: int, random_state: int) -> str:
     logging.warning(f"train {x_train_path} {y_train_path}")
 
-    # TODO : Implémenter la fonction avec les expérimentations du notebook
+    # fonction avec les expérimentations du notebook
+    x_train = pd.read_csv(x_train_path, index_col=False)
+    y_train = pd.read_csv(y_train_path, index_col=False)
+
+    x_train = pd.get_dummies(x_train)
+
+    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)
+    model.fit(x_train, y_train)
+
+    model_filename = "model.joblib"
+
+    model_path = Path("./dist/", model_filename)
+    joblib.dump(model, model_path)
+
+
+    return model_path
+
+
+
     # TODO : Dans un second temps, récupérer les données depuis mlflow
 
     # TODO : Dans un second temps, stocker le model en tant qu'artifact dans mlflow
