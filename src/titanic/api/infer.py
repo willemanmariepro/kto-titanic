acb729e5f9bdea711471c3988e0da6c9a4298eaf
willemanmariepro <Willeman.marie.pro@gmail.com>
2026-03-30 13:51:24 +0000

    feat : api : authentification, pickle, infer

--- src/titanic/api/infer.py
+++ src/titanic/api/infer.py
@@ -5,11 +5,16 @@ ou directement par un front. Remplir ce script une fois l'entrainement du model 
 """
 
 import os
+import pickle
 
-# TODO : Importer les dépendances utiles au bon développement en Python (dataclass, enum, pandas)
-# TODO : Importer les dépendances pour sérialiser / désérialiser le model
+# DONE: Importer les dépendances utiles au bon développement en Python (dataclass, enum, pandas)
+# DONE : Importer les dépendances pour sérialiser / désérialiser le model
+from dataclasses import dataclass
+from enum import Enum
+import pandas as pd
+# DONE : Importer les dépendances fastAPI
+from fastapi import FastAPI,Depends
 
-# TODO : Importer les dépendances fastAPI
 # TODO : Importer les dépendances OTEL pour le monitoring
 
 from titanic.api.auth import verify_token
@@ -20,22 +25,50 @@ JAEGER_ENDPOINT = os.getenv("JAEGER_ENDPOINT", "http://jaeger.willemanmariepro-d
 # TODO : Intégrer les configurations d'OTEL et instancier le tracer. Peut être fait plus tard si le cours
 # sur l'observabilité n'est pas encore donné
 
-# TODO : Instancier l'application FastAPI
+# DONE : Instancier l'application FastAPI
+app = FastAPI()
+# DONE : Ouvrir et charger en mémoire le pickle qui sérialise le model
+with open("./src/titanic/api/resources/model.pkl", "rb") as f:
+    model = pickle.load(f)
+# DONE : Créer les class et dataclass représentant la donnée qui sera transmise au Webservice pour l'inférence
+# DONE : Créer Pclass (enum)
+# DONE : Créer Sex (enum)
+# DONE : Créer Passenger (attention, l'objet doit pouvoir être transmis en dictionnaire au model. Il faudra créer une méthode d'instance
+class Pclass(Enum):
+    UPPER = 1
+    MIDDLE = 2
+    LOW = 3
 
-# TODO : Ouvrir et charger en mémoire le pickle qui sérialise le model
 
-# TODO : Créer les class et dataclass représentant la donnée qui sera transmise au Webservice pour l'inférence
+class Sex(Enum):
+    MALE = "male"
+    FEMALE = "female"
 
-# TODO : Créer Pclass (enum)
-# TODO : Créer Sex (enum)
-# TODO : Créer Passenger (attention, l'objet doit pouvoir être transmis en dictionnaire au model. Il faudra créer une méthode d'instance
 
+@dataclass
+class Passenger:
+    pclass: Pclass
+    sex: Sex
+    sibSp: int
+    parch: int
+
+    def to_dict(self) -> dict:
+        return {"Pclass": self.pclass.value, "Sex": self.sex.value, "SibSp": self.sibSp, "Parch": self.parch}
+    
 # TODO : Faire en sorte que cette fonction soit exposée via une toute GET /health
+@app.get("/health")
 def health() -> dict:
     return {"status": "OK"}
 
-# TODO : Faire en sorte que cette fonction soit exposée via une route POST /infer
-# TODO : Ajouter les paramètres de la fonction (peut se faire en deux fois avec la sécurisation via oAuth2)
-def infer() -> list:
-    # TODO : implémenter le corps de la fonction
-    return [0]
+# DONE : Ajouter les paramètres de la fonction (peut se faire en deux fois avec la sécurisation via oAuth2)
+@app.post("/infer")
+def infer(passenger: Passenger, token: str = Depends(verify_token("api:read"))) -> list:
+
+    df_passenger = pd.DataFrame([passenger.to_dict()])
+    df_passenger["Sex"] = pd.Categorical(df_passenger["Sex"], categories=[Sex.FEMALE.value, Sex.MALE.value])
+    df_to_predict = pd.get_dummies(df_passenger)
+
+    res = model.predict(df_to_predict)
+
+    return res.tolist()
+
